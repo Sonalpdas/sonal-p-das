a649f6f4a894c13520852449e8f6fe2fe48a2a28
Sonal Das <sonald@alumni.cmu.edu>
2020-12-02 16:47:04 -0500

    Create updateReadme.js
    Added updateReadme.js for Workflow

---
+++ updateReadme.js
@@ -0,0 +1,31 @@
+// Include node fs (file stream) and https modules
+const fs = require('fs');
+
+function readWriteAsync() {
+  
+  // Update README using FS
+      fs.readFile('README.md', 'utf-8', (err, data) => {
+        if (err) {
+          throw err;
+        }
+
+        let currImage = "<img src="https://media.giphy.com/media/oy83DwqHRcR1jJczV3/giphy.gif" align="right" alt="Coder GIF" width="180" height="180">";
+        let newImage = "<img src="https://media.giphy.com/media/L8K62iTDkzGX6/giphy.gif" align="right" alt="Coder GIF" width="180" height="180">";
+        
+        const updatedMd = data.replace(currImage,
+          newImage
+        );
+
+        // Write the new README
+        fs.writeFile('README.md', updatedMd, 'utf-8', (err) => {
+          if (err) { 
+            throw err;
+          }
+
+          console.log('README update complete.');
+        });
+      });
+}
+
+// Call the function
+readWriteAsync();
